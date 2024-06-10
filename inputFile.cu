57b9b5e522699988879683f99866909327aef6f1
Rakshit1110 <155001695+Rakshit1110@users.noreply.github.com>
2024-06-10 11:22:20 +0530

    Update inputFile.cu

---
+++ inputFile.cu
@@ -0,0 +1,15 @@
+int func(int a, char *s){
+    char *c=s;
+    int b=a;
+    return b;
+}
+int main(){
+    /* NO ERROR SHOULD COME IN THIS FILE*/
+    while(a<b){
+        i=i+1;
+    }
+    /* Comment handling is fine */
+    char *s="Hello";
+    func(a,s);
+    return 0;
+}
